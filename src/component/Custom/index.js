df7787ddf1c5c1173b1ecf5ffd75c38288183fa9
apurv-dolphin <apurv.dolphinwebsolution@gmail.com>
2022-10-14 10:37:12 +0530

    override thr component

--- src/component/Custom/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Custom() {
-	return (
-	  <button>Mover</button>
-	)
-}
-
---
+++ src/override/@magento/venia-ui/lib/components/Footer/footer.js
@@ -0,0 +1,148 @@
+import React, { Fragment } from 'react';
+import { Facebook, Instagram, Twitter, GitHub, Linkedin } from 'react-feather';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { Link } from 'react-router-dom';
+import { shape, string } from 'prop-types';
+import { useFooter } from '@magento/peregrine/lib/talons/Footer/useFooter';
+import defaultClasses from '@magento/venia-ui/lib/components/Footer/footer.module.css';
+import resourceUrl from '@magento/peregrine/lib/util/makeUrl';
+import { useStyle } from '@magento/venia-ui/lib/classify';
+import Newsletter from '@magento/venia-ui/lib/components/Newsletter';
+import Logo from '@magento/venia-ui/lib/components/Logo';
+import { DEFAULT_LINKS, LOREM_IPSUM } from './sampleData';
+
+const Footer = props => {
+    const { links } = props;
+    const classes = useStyle(defaultClasses, props.classes);
+    const talonProps = useFooter();
+
+    const { copyrightText } = talonProps;
+    const { formatMessage } = useIntl();
+    const title = formatMessage({ id: 'logo.title', defaultMessage: 'Venia' });
+
+    const linkGroups = Array.from(links, ([groupKey, linkProps]) => {
+        const linkElements = Array.from(linkProps, ([text, pathInfo]) => {
+            let path = pathInfo;
+            let Component = Fragment;
+            if (pathInfo && typeof pathInfo === 'object') {
+                path = pathInfo.path;
+                Component = pathInfo.Component;
+            }
+
+            const itemKey = `text: ${text} path:${path}`;
+            const child = path ? (
+                <Link data-cy="Footer-link" className={classes.link} to={path}>
+                    <FormattedMessage id={text} defaultMessage={text} />
+                </Link>
+            ) : (
+                <span data-cy="Footer-label" className={classes.label}>
+                    <FormattedMessage id={text} defaultMessage={text} />
+                </span>
+            );
+
+            return (
+                <Component key={itemKey}>
+                    <li className={classes.linkItem}>{child}</li>
+                </Component>
+            );
+        });
+
+        return (
+            <ul key={groupKey} className={classes.linkGroup}>
+                {linkElements}
+            </ul>
+        );
+    });
+
+    return (
+        <footer data-cy="Footer-root" className={classes.root}>
+            <div className={classes.links}>
+                {linkGroups}
+                <div className={classes.callout}>
+                    <span
+                        data-cy="Footer-calloutHeading"
+                        className={classes.calloutHeading}
+                    >
+                        <FormattedMessage
+                            id={'footer.followText'}
+                            defaultMessage={'Follow Us!'}
+                        />
+                    </span>
+                    <p
+                        data-cy="Footer-calloutText"
+                        className={classes.calloutBody}
+                    >
+                        <FormattedMessage
+                            id={'footer.calloutText'}
+                            defaultMessage={LOREM_IPSUM}
+                        />
+                    </p>
+                    <ul className={classes.socialLinks}>
+                        <li>
+                            <a href="https://www.instagram.com/accounts/login/">
+                                <Instagram size={20} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/">
+                                <Facebook size={20} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/login">
+                                <Twitter size={20} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/topics/pwa">
+                                <GitHub size={20} />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://in.linkedin.com/">
+                                <Linkedin size={20} />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <Newsletter />
+            </div>
+            <div className={classes.branding}>
+                <ul className={classes.legal}>
+                    <li data-cy="Footer-terms" className={classes.terms}>
+                        <FormattedMessage
+                            id={'footer.termsText'}
+                            defaultMessage={'Terms of Use'}
+                        />
+                    </li>
+                    <li data-cy="Footer-privacy" className={classes.privacy}>
+                        <FormattedMessage
+                            id={'footer.privacyText'}
+                            defaultMessage={'Privacy Policy'}
+                        />
+                    </li>
+                </ul>
+                <p className={classes.copyright}>{copyrightText || null}</p>
+                <Link
+                    to={resourceUrl('/')}
+                    aria-label={title}
+                    className={classes.logoContainer}
+                >
+                    <Logo classes={{ logo: classes.logo }} />
+                </Link>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
+
+Footer.defaultProps = {
+    links: DEFAULT_LINKS
+};
+
+Footer.propTypes = {
+    classes: shape({
+        root: string
+    })
+};
